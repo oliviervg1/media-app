910c8ef7b13b00c592f5db64a66ee122991cd82d
Olivier Van Goethem <ov109@ic.ac.uk>
2013-05-21 15:18:31 +0100

    Initial code commit

---
+++ src/main/java/com/pi/MediaApp.java
@@ -0,0 +1,120 @@
+package com.pi;
+
+import java.io.File;
+import java.net.URL;
+import java.util.HashMap;
+
+import javax.xml.namespace.QName;
+
+import com.pi.ressources.XMLEditor;
+
+import automation.api.AbstractApp;
+
+public class MediaApp extends AbstractApp {
+
+	private XMLEditor xml = new XMLEditor("/home/pi/FYP/apache-tomcat-7.0.35/webapps/assets/osmplayer/playlist.xml"); 
+	
+	@Override
+	public void onStartup() {
+		connectToRemoteDevice("http://192.168.0.234:8080/media-companion-1.0.0/MediaPlayer?wsdl", new QName("http://media.pi.com/", "MediaPlayerService"));
+	}
+	
+	@Override
+	public String getState() throws NoSuchMethodException {
+		if (isDeviceAvailable()) {
+			return (String) device.invokeMethod("getPlayingFile");
+		} else {
+			return "Media device unavailable!";
+		}
+	}
+
+	@Override
+	public String homeTile() throws NoSuchMethodException {
+		if (isDeviceAvailable()) {
+			return (String) device.invokeMethod("getPlayingFile");
+		} else {
+			return "Media device unavailable!";
+		}
+	}
+	
+	@Override
+	public void uploadFile(String fileName, File file) {
+		xml.addTrack(fileName, "http://www.stuart-holland.com:8080/uploads/" + file.getName(), "HTML5");
+	}
+	
+	@Override
+	public HashMap<String, Object> getModels() {
+		HashMap<String, Object> models = new HashMap<String, Object>();
+		models.put("tracks", xml.getTrackList());
+		return models;
+	}
+	
+	public void addTrack(String title, String location, String type) {
+		xml.addTrack(title, location, type);
+	}
+	
+	public void removeTrack(String id) {
+		URL locationToRemove = xml.removeTrack(id);
+		File fileToRemove = new File("/home/pi/FYP/apache-tomcat-7.0.35/webapps" + locationToRemove.getPath());
+		if (fileToRemove.isFile()) {
+			fileToRemove.delete();
+		}
+	}
+	
+	public void playTrack(String id) throws NoSuchMethodException {
+		String fileToPlay = xml.getTrackLocation(id);
+		fileToPlay = fileToPlay.replace(" ", "%20");
+		Object parameters[] = {fileToPlay};
+		if (isDeviceAvailable()) {
+			device.invokeMethod("playTrack", parameters);
+		}
+	}
+	
+	public void togglePlay() throws NoSuchMethodException {
+		if (isDeviceAvailable()) {
+			device.invokeMethod("togglePlay");
+		}
+	}
+	
+	public void setVolume(float volume) throws NoSuchMethodException {
+		Object parameters[] = {volume};
+		if (isDeviceAvailable()) {
+			device.invokeMethod("setVolume", parameters);
+		}
+	}
+	
+	public float getVolume() throws NoSuchMethodException {
+		if (isDeviceAvailable()) {
+			return (float) device.invokeMethod("getVolume");
+		} else {
+			return 0;
+		}
+	}
+	
+	public void setTimePosition(long seconds) throws NoSuchMethodException {
+		Object parameters[] = {seconds};
+		if (isDeviceAvailable()) {
+			device.invokeMethod("setTimePosition", parameters);
+		}
+	}
+	
+	public long getTimePosition() throws NoSuchMethodException {
+		if (isDeviceAvailable()) {
+			return (long) device.invokeMethod("getTimePosition");
+		}
+		return 0;
+	}
+	
+	public long getTotalTime() throws NoSuchMethodException {
+		if (isDeviceAvailable()) {
+			return (long) device.invokeMethod("getTotalTime");
+		}
+		return 0;
+	}
+	
+	public void close() throws NoSuchMethodException {
+		if (isDeviceAvailable()) {
+			device.invokeMethod("close");
+		}
+	}
+}
---
+++ src/main/java/com/pi/ressources/XMLEditor.java
@@ -0,0 +1,128 @@
+package com.pi.ressources;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.jdom2.Document;
+import org.jdom2.Element;
+import org.jdom2.JDOMException;
+import org.jdom2.Namespace;
+import org.jdom2.input.SAXBuilder;
+import org.jdom2.output.Format;
+import org.jdom2.output.XMLOutputter;
+ 
+public class XMLEditor {
+	
+	private String file;
+	private SAXBuilder builder;
+	private XMLOutputter xmlOutput;
+	private Document doc;
+
+	public XMLEditor(String file) {
+		this.file = file;
+		builder = new SAXBuilder();
+		xmlOutput = new XMLOutputter();
+		xmlOutput.setFormat(Format.getPrettyFormat());
+		doc = null;
+	}
+	
+	private void loadXML() {
+		File xmlFile = new File(file); 
+		try {
+			doc = (Document) builder.build(xmlFile);
+		} catch (JDOMException | IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void outputXML() {
+		try {
+			xmlOutput.output(doc, new FileWriter(file));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public ArrayList<String> getTrackList() {
+		loadXML();
+		Element rootNode = doc.getRootElement();
+		Namespace ns = rootNode.getNamespace();
+		Element trackList = rootNode.getChild("trackList", ns);
+		
+		ArrayList<String> trackNames = new ArrayList<String>();
+		for (Element track : trackList.getChildren()) {
+			trackNames.add(track.getAttributeValue("id"));
+		}
+		
+		return trackNames;
+	}
+	
+	public String getTrackLocation(String trackId) {
+		loadXML();
+		Element rootNode = doc.getRootElement();
+		Namespace ns = rootNode.getNamespace();
+		Element trackList = rootNode.getChild("trackList", ns);
+		
+		for (Element track : trackList.getChildren()) {
+			if (track.getAttribute("id").getValue().equalsIgnoreCase(trackId)) {
+				return track.getChild("location", ns).getText();
+			}
+		}
+		
+		return null;
+	}
+	
+	public void addTrack(String title, String location, String type) {
+		loadXML();
+		Element rootNode = doc.getRootElement();
+		Namespace ns = rootNode.getNamespace();
+		Element trackList = rootNode.getChild("trackList", ns);
+		
+		Element elementToAdd = new Element("track", ns);
+		elementToAdd.setAttribute("id", title);
+		elementToAdd.addContent(new Element("title", ns).setText(type + ": " + title));
+		elementToAdd.addContent(new Element("location", ns).setText(location));
+			
+		trackList.addContent(elementToAdd);
+		outputXML();
+	}
+	
+	public URL removeTrack(String id) {
+		loadXML();
+		Element rootNode = doc.getRootElement();
+		Namespace ns = rootNode.getNamespace();
+		Element trackList = rootNode.getChild("trackList", ns);
+		
+		// Get list of tracks
+		List<Element> tracks = trackList.getChildren();
+		
+		// Removing elements
+		Element elementToRemove = null;
+			
+		for (Element element : tracks) {
+			if(element.getAttribute("id").getValue().equalsIgnoreCase(id)) {
+				elementToRemove = element;
+			}
+		}
+		
+		URL filepathToDelete = null;
+		try {
+			filepathToDelete = new URL(elementToRemove.getChild("location", ns).getValue());
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		trackList.removeContent(elementToRemove);
+		outputXML();
+		
+		return filepathToDelete;
+	}
+}
